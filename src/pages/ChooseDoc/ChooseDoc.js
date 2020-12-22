7bd25819e4e9dfeaf5b682c1a7f7968f9dfe555e
Konstantin Front <frontkot@gmail.com>
2020-12-22 18:15:54 +0200

    deploy for show client

--- src/pages/ChooseDoc/ChooseDoc.js
+++ src/pages/ChooseDoc/ChooseDoc.js
@@ -5,7 +5,7 @@ const ChooseDoc = () => {
     return (
         <>
             <DocumentPreview textContent='Акт выполненых работ' link={'/act'} className='doc-review' />
-            <DocumentPreview textContent='Счет на оплату' link={'/payment'} className='doc-review' />
+            {/* <DocumentPreview textContent='Счет на оплату' link={'/payment'} className='doc-review' /> */}
             <DocumentPreview textContent='Счет-фактура' link={'/invoice'} className='doc-review' />
         </>
     );
